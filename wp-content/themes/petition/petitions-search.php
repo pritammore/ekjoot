da18f092ad2f59b5e82acf97378bd93fa1f20615
Pritam More <pritammore.d@gmail.com>
2017-10-16 00:44:58 +0530

    Enabled UIC search in main search box.
    Added Query to search in postmeta for petition_uic yet to show UIC code in suggesstions and search result.

--- wp-content/themes/petition/petitions-search.php
+++ wp-content/themes/petition/petitions-search.php
@@ -15,6 +15,18 @@ $minimum_signature = isset($conikal_general_settings['conikal_minimum_signature_
 
 $keyword = isset($_GET['q']) ? sanitize_text_field($_GET['q']) : '';
 
+global $wpdb;
+$uic_petitions = $uic_search_Ids = array();
+$uic_query_string = "SELECT * 
+                    FROM $wpdb->postmeta 
+                    WHERE meta_key = 'petition_uic' 
+                    AND meta_value LIKE '%$keyword%'";
+
+foreach ($wpdb->get_results( $uic_query_string ) as $key => $row) {
+    $uic_search_Ids[] = $row->post_id;
+    $uic_petitions[] = get_petition_search_result(get_post($row->post_id));
+}
+
 $args = array(
     'post_type' => 'petition',
     'posts_per_page' => -1,
@@ -32,6 +44,7 @@ if ($minimum_signature != 0) {
     ));
 }
 
+
 $petitions = new WP_Query($args);
 wp_reset_query();
 wp_reset_postdata();
@@ -39,92 +52,115 @@ wp_reset_postdata();
 $arrayPetitions = array();
 if($petitions->have_posts()) {
     while ( $petitions->have_posts() ) {
+        $proceed = true;
         $petitions->the_post();
         $id = get_the_ID();
-        $link = get_permalink($id);
-        $title = get_the_title($id);
-        $category =  wp_get_post_terms($id, 'petition_category', true);
-        $excerpt = conikal_get_excerpt_by_id($id);
-        $comments = wp_count_comments($id);
-        $comments_fomated = conikal_format_number('%!,0i', $comments->approved, true);
-        $gallery = get_post_meta($id, 'petition_gallery', true);
-        $images = explode("~~~", $gallery);
-        $address = get_post_meta($id, 'petition_address', true);
-        $city = get_post_meta($id, 'petition_city', true);
-        $state = get_post_meta($id, 'petition_state', true);
-        $neighborhood = get_post_meta($id, 'petition_neighborhood', true);
-        $zip = get_post_meta($id, 'petition_zip', true);
-        $country = get_post_meta($id, 'petition_country', true);
-        $lat = get_post_meta($id, 'petition_lat', true);
-        $lng = get_post_meta($id, 'petition_lng', true);
-        $receiver = get_post_meta($id, 'petition_receiver', true);
-        $receiver = explode(',', $receiver);
-        $position = get_post_meta($id, 'petition_position', true);
-        $position = explode(',', $position);
-        $goal = get_post_meta($id, 'petition_goal', true);
-        $sign = get_post_meta($id, 'petition_sign', true);
-        $sign_fomated = conikal_format_number('%!,0i', $sign);
-        $sign_compact = conikal_format_number('%!,0i', $sign, true);
-        $updates = get_post_meta($id, 'petition_update', true);
-        $thumb = get_post_meta($id, 'petition_thumb', true);
-        $thumb = conikal_video_thumbnail($thumb);
-        $status = get_post_meta($id, 'petition_status', true);
-        $author_link = get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ));
-
-        $user_avatar = get_the_author_meta('avatar' , get_the_author_meta('ID'));
-        if($user_avatar != '') {
-            $avatar = $user_avatar;
-        } else {
-            $avatar = get_template_directory_uri().'/images/avatar.svg';
-        }
-        $avatar = conikal_get_avatar_url( get_the_author_meta('ID'), array('size' => 28, 'default' => $avatar) );
-
-        if(has_post_thumbnail()) {
-            $thumb_id = get_post_thumbnail_id();
-            $thumbnail = wp_get_attachment_image_src($thumb_id, 'petition-thumbnail', true);
-            $thumbnail = $thumbnail[0];
-        } elseif ($gallery) {
-            $thumbnail = $images[1];
-        } elseif ($thumb) {
-            $thumbnail = $thumb;
-        } else {
-            $thumbnail = get_template_directory_uri() . '/images/thumbnail.svg';
+        if(!empty($uic_search_Ids)) {
+            foreach($uic_search_Ids as $uic_Id)
+            {
+                if($uic_Id == $id) {
+                    $proceed = false;
+                    break;
+                }
+                else {
+                    $proceed = true;
+                }
+            }
         }
 
-        $arrayPetition = array(
-                'id' => $id, 
-                'link' => $link,
-                'title' => $title,
-                'category_name' => $category[0]->name,
-                'category_link' => get_category_link($category[0]->term_id),
-                'excerpt' => $excerpt,
-                'comments' => $comments->approved,
-                'comments_fomated' => $comments_fomated,
-                'address' => $address,
-                'city' => $city,
-                'state' => $state,
-                'neighborhood' => $neighborhood,
-                'zip' => $zip,
-                'country' => $country,
-                'lat' => $lat,
-                'lng' => $lng,
-                'receiver' => $receiver[0],
-                'position' => $position[0],
-                'goal' => $goal,
-                'updates' => $updates,
-                'thumbnail' => $thumbnail,
-                'status' => $status,
-                'author_avatar' => $avatar,
-                'author_name' => get_the_author(),
-                'author_link' => $author_link,
-                'sign' => $sign,
-                'sign_fomated' => $sign_fomated,
-                'sign_compact' => $sign_compact . __(' supporters', 'petition')
-            );
+        if($proceed)
+        {
+            $link = get_permalink($id);
+            $title = get_the_title($id);
+            $category =  wp_get_post_terms($id, 'petition_category', true);
+            $excerpt = conikal_get_excerpt_by_id($id);
+            $comments = wp_count_comments($id);
+            $comments_fomated = conikal_format_number('%!,0i', $comments->approved, true);
+            $gallery = get_post_meta($id, 'petition_gallery', true);
+            $images = explode("~~~", $gallery);
+            $address = get_post_meta($id, 'petition_address', true);
+            $city = get_post_meta($id, 'petition_city', true);
+            $state = get_post_meta($id, 'petition_state', true);
+            $neighborhood = get_post_meta($id, 'petition_neighborhood', true);
+            $zip = get_post_meta($id, 'petition_zip', true);
+            $country = get_post_meta($id, 'petition_country', true);
+            $lat = get_post_meta($id, 'petition_lat', true);
+            $lng = get_post_meta($id, 'petition_lng', true);
+            $receiver = get_post_meta($id, 'petition_receiver', true);
+            $receiver = explode(',', $receiver);
+            $position = get_post_meta($id, 'petition_position', true);
+            $position = explode(',', $position);
+            $goal = get_post_meta($id, 'petition_goal', true);
+            $sign = get_post_meta($id, 'petition_sign', true);
+            $sign_fomated = conikal_format_number('%!,0i', $sign);
+            $sign_compact = conikal_format_number('%!,0i', $sign, true);
+            $updates = get_post_meta($id, 'petition_update', true);
+            $thumb = get_post_meta($id, 'petition_thumb', true);
+            $thumb = conikal_video_thumbnail($thumb);
+            $status = get_post_meta($id, 'petition_status', true);
+            $petition_uic = get_post_meta($id, 'petition_uic', true);
+            $author_link = get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ));
 
-        $arrayPetition = (object) $arrayPetition;
-        array_push($arrayPetitions, $arrayPetition);
+            $user_avatar = get_the_author_meta('avatar' , get_the_author_meta('ID'));
+            if($user_avatar != '') {
+                $avatar = $user_avatar;
+            } else {
+                $avatar = get_template_directory_uri().'/images/avatar.svg';
+            }
+            $avatar = conikal_get_avatar_url( get_the_author_meta('ID'), array('size' => 28, 'default' => $avatar) );
+
+            if(has_post_thumbnail()) {
+                $thumb_id = get_post_thumbnail_id();
+                $thumbnail = wp_get_attachment_image_src($thumb_id, 'petition-thumbnail', true);
+                $thumbnail = $thumbnail[0];
+            } elseif ($gallery) {
+                $thumbnail = $images[1];
+            } elseif ($thumb) {
+                $thumbnail = $thumb;
+            } else {
+                $thumbnail = get_template_directory_uri() . '/images/thumbnail.svg';
+            }
+
+            $arrayPetition = array(
+                    'id' => $id, 
+                    'link' => $link,
+                    'title' => $title,
+                    'category_name' => $category[0]->name,
+                    'category_link' => get_category_link($category[0]->term_id),
+                    'excerpt' => $excerpt,
+                    'comments' => $comments->approved,
+                    'comments_fomated' => $comments_fomated,
+                    'address' => $address,
+                    'city' => $city,
+                    'state' => $state,
+                    'neighborhood' => $neighborhood,
+                    'zip' => $zip,
+                    'country' => $country,
+                    'lat' => $lat,
+                    'lng' => $lng,
+                    'receiver' => $receiver[0],
+                    'position' => $position[0],
+                    'goal' => $goal,
+                    'updates' => $updates,
+                    'thumbnail' => $thumbnail,
+                    'status' => $status,
+                    'author_avatar' => $avatar,
+                    'author_name' => get_the_author(),
+                    'author_link' => $author_link,
+                    'sign' => $sign,
+                    'sign_fomated' => $sign_fomated,
+                    'sign_compact' => $sign_compact . __(' supporters', 'petition'),
+                    'petition_uic' => $petition_uic
+                );
+
+            $arrayPetition = (object) $arrayPetition;
+            array_push($arrayPetitions, $arrayPetition);
+        }
     }
+}
+
+if ($uic_petitions) {
+    $arrayPetitions = array_merge($arrayPetitions,$uic_petitions);
 }
 
 $args = array(
@@ -145,7 +181,8 @@ wp_reset_postdata();
 wp_reset_query();
 
 if ($arrayPetitions) {
-    echo json_encode(array('status' => true, 'total' => $petitions->found_posts, 'results' => $arrayPetitions, 'message' => __('Petitions was loaded successfully.', 'petition'), 'action' => array('url' => $page_link, 'text' => __('View ', 'petition') . $petitions->found_posts . __(' results for ', 'petition') . '"' . $keyword . '"' ) ));
+    $found_posts = count($arrayPetitions);
+    echo json_encode(array('status' => true, 'total' => $found_posts, 'results' => $arrayPetitions, 'message' => __('Petitions was loaded successfully.', 'petition'), 'action' => array('url' => $page_link, 'text' => __('View ', 'petition') . $found_posts . __(' results for ', 'petition') . '"' . $keyword . '"' ) ));
     exit();
 } else {
     echo json_encode(array('status' => false, 'message' => __('Something went wrong. Not found petitions.', 'petition')));
